e8cb68ee88065750ceb1096070ae78a563f5d885
viatoro <jarupong.wiboonchart@gmail.com>
2021-01-25 09:17:51 +0700

    add mas

--- system/install.sh
+++ system/install.sh
@@ -8,8 +8,10 @@ brew install --cask playonmac
 brew install steam
 brew install awsume
 brew install zoom
-brew install slack
 brew install --cask microsoft-teams
 brew install --cask tunnelblick
 brew install awscli
 brew install jq
+brew install mas
+mas install 803453959 || mas purchase 803453959
+mas install 539883307 || mas purchase 539883307
