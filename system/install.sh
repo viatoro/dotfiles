7cbde4dc32ad3752c6fd67b28f64284bb3ed32f5
jarupong <jarupong.wiboonchart@krungsri.com>
2021-02-13 20:58:15 +0700

    add new

--- system/install.sh
+++ system/install.sh
@@ -2,22 +2,11 @@
 #
 
 set -e
-brew install --cask  xquartz
-brew install wget
-brew install --cask playonmac
-brew install steam
-brew install awsume
-brew install zoom
-brew install --cask microsoft-teams
-brew install --cask tunnelblick
-brew install awscli
-brew install jq
-brew install mas
-brew install --cask google-chrome
-brew install --cask brave-browser
-brew install coreutils
-brew install --cask intellij-idea
-brew install --cask sourcetree
-brew install --cask postman
+brew install -q wget awsume jq mas coreutils
+if test ! $(which aws)
+then
+  brew install awscli
+fi
+brew install --cask -q steam zoom xquartz playonmac microsoft-teams tunnelblick google-chrome brave-browser intellij-idea sourcetree postman robo-3t
 mas install 803453959 || mas purchase 803453959
 mas install 539883307 || mas purchase 539883307
