c20132d6f00e3af05a6a6740d90956aba09ef13c
jarupong <jarupong.wiboonchart@krungsri.com>
2022-02-11 11:51:20 +0700

    modifly flutter

---
+++ flutter/path.zsh
@@ -0,0 +1,8 @@
+export FLUTTERPATH=/opt/flutter/
+export PATH="$FLUTTERPATH/bin:$PATH"
+export ANDROID_HOME=/usr/local/share/android-sdk
+export JAVA_HOME=/Library/Java/JavaVirtualMachines/adoptopenjdk-8.jdk/Contents/Home
+export PATH=$ANDROID_HOME/emulator:$PATH
+#export PATH=”$PATH:`pwd`/flutter/bin”
+#export PATH=”$PATH:`pwd`/bin/cache/dart-sdk/bin”
+#export PATH=”$PATH:`pwd`/.pub-cache/bin”
