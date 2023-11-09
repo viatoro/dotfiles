99a5faf893e207a5cd90c8593342b1c53da8af9f
viatoro <jarupong.wiboonchart@gmail.com>
2023-11-09 13:57:39 +0700

    Update install.sh

--- pipx/install.sh
+++ pipx/install.sh
@@ -4,12 +4,8 @@
 set -e
 
 set -e
-if test ! $(which pipx)
+if test $(which pipx)
 then
   pipx install awsume
   pipx install aws-sso-credential-process
-  # if test $(which brew)
-  # then
-  #   sudo npm install spoof -g
-  # fi
 fi
